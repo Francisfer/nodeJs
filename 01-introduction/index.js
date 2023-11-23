95df0bf96f5ab246c032e821213146c35d05c1cb
Francisfer <114183517+Francisfer@users.noreply.github.com>
2023-11-23 19:07:10 +0000

    Progress added

--- 01-introduction/index.js
+++ 01-introduction/index.js
@@ -1,4 +1,16 @@
 /*
+VSCODE EXTENSIONS 
+
+DotENV - useful to highlight environmental variables in a dotenv file.
+
+ESlint - useful to find bugs in our code.
+
+Pug beautify - useful when building templates.
+
+TabNine - auto completer.
+
+TODO highlight
+
 INITIAL COMMANDS
   - Node on terminal opens a repl (read, eval, print, loop)
 
@@ -8,15 +20,69 @@ INITIAL COMMANDS
 
   - To run a script instead of a repl we need to specify the file, in this case index.js - node index.js
 
-DEPENDENCIES
-
-
-
 MODULES
 
 1. const fs = require("fs");
 2. const http = require("http");
 3. const url = require("url");
+
+EXPORTS AND IMPORTS
+Every js file is a module that can import or export functionality. In this example we exported the replaceTemplate function to another module and then imported it here.
+
+There are different ways for exporting functionality from a module, we are going to talk about this later, for now, in node, we can achieve this with module.exports
+
+In each module we have access to an object called module, on which we can set the exports property. Then we set it to what we want to export.
+
+In this case it is just one function so he used an anonymous export, so we can give a name when we import it, but there must be a way to make named exports.
+
+INTRODUCTION TO NPM AND THE PACKAGE.JSON FILE //////////////////////////////
+Whenever we start a new project, the first thing that we usually do is to start it with npm init. 
+
+NPM INIT - Creates the package.json.
+         - The package name should always be url friendly.
+
+DEPENDENCIES/INSTALLS/PACKAGES
+REGULAR: npm i nameOfThePackage (--save previously but no longer necessary)
+
+slugify - helps with the names on the url's. A slug is the last part of a url that contains a unique string that identifies the resource that the website is displaying (THE id). This is to mask that id for another name.
+console.log(slugify("Fresh Avocados", { lower: true }));
+By using slugify directly in our code we make it dependent of that module.
+
+DEV DEPENDENCIES: npm i nameOfThePackage --save-dev
+
+nodemon (helps to develop node applications by automatically restarting the server (node application) whenever we make changes to the code - nodemon index.js).
+
+
+1. The two types of packages that we can install are simple/regular dependencies and development dependencies.  
+
+    1.1 - Simple or regular dependencies are packages that contain some code that we will include in our own code. In other words, code upon we built our own application (express).
+    The first regular dependency that we are going to install is slugify (tool that we can use to make more readable url's out of names).
+
+    1.2 - Development dependencies are usually just tools for development (code bundler like webpack, a debugger tool or a testing library).
+    These dependencies are not needed for production, the code does not really depend on them but we use them to develop our applications.
+    The first that we install in nodemon.
+
+2. We can install packages locally or globally, nodemon for ex is like live server, we can install it globally because we need it for all node projects.
+However, most of the dependencies, especially the regular ones, are advised to be installed locally only.
+Another difference is that, if we install a dependency locally, we can't just run it right of the box from the command line, we need a npm script. After the command is specified in the script, we can run it in the command line with npm run "nameOfTheScript".
+start - the default for development, that's why we don't need the run keyword, npm start in enough.
+
+3. We've already imported the core modules, our own modules and now the third party modules.
+The order is exactly that one. The process is the same, we require the module and save the result into a variable. Requiring is simply going to the node modules and fetch what we are asking for.
+The core modules didn't need a script because we have node installed globally, so every module that is part of the core don't need to be in the script.
+Everything else needs because we are installing the dependencies locally, so we need the node modules to have this data when we require.
+
+4. Dependencies version: "slugify": "^1.6.6"
+The first number is the major version (huge new release which can have breaking changes - our code can break).
+The second is the minor version (introduces some new features into the package but does not include any breaking changes - meaning that changes made here will always be backward compatible - our code does not break) and the third is the patch version(only to fix bugs).
+
+    4.1 - Because of this, it is important to talk about updating packages. The ^ symbol specifies which updates we accept for each package. It's default means that we accept minor and patch releases. If we just want patch releases the symbol is ~ (a bit safer), if we want them all is * (not a good idea).
+
+    UPDATE packages. The first thing we do is to see if there is any package that is outdated with npm outdated. We do not have any outdated, but he installs a previous version of slugify to show how we can update.
+    To install a specific version we write npm i slugify@1.0.0
+    To update we do npm update slugify (which depends on the updates that we accept ~ or ^).
+
+    DELETE packages - npm uninstall express
 
 
 
@@ -93,12 +159,18 @@ Remember that arrow functions do not have their own this keyword, they use the t
 
 */
 
-// Sync
+// Core modules
 
 const fs = require("fs");
 const http = require("http");
 const { json } = require("stream/consumers");
 const url = require("url");
+
+// Third party modules
+const slugify = require("slugify");
+// Our own modules. Notice that we don't need the .js
+
+const replaceTemplate = require("./modules/replaceTemplate");
 
 //////////////////////////////////////////////// FILES
 /* UNCOMMENT
@@ -271,7 +343,7 @@ fs.readFile(`${__dirname}/dev-data/data.json`);
 The first thing is to parse the json format to javascript. const productData = JSON.parse(data);
 We are not going to send this as a response now, but we will later need this parsed so we can work with the templates.
 
-So, if we want to send the data as a response we need to tell the browser that data goes under the format of json (we need to say application/json).
+So, if we want to SEND the data as a response we need to tell the browser that data goes under the format of json (we need to say application/json).
 const productData = JSON.parse(data);
       res.writeHead(200, {
         "Content-type": "application/json",
@@ -305,17 +377,73 @@ After this we can delete all the cards from the overview-template and create a p
 Later in our code we will create as many cards as we need, put them into one string and replace the placeholder with that string.
 Now we replace the data with the placeholder, notice that also we need to work with the organic class and we need to build the href of the link.
 
-FILLING THE TEMPLATES LESSON 15
+FILLING THE TEMPLATES 
 
-We are starting with the overview page and, the first step is to load the template overview. 
+We are starting with the overview page and, the first step is to load/read the template overview. 
 This is the same as saying that, each time there is a request for the root or the overview route, the first thing that we are going to do is to read the template overview.
 Again, we can do this right at the beginning when we start the application because the templates will always be the same.
 
+For the root and overview route we are sending data, remember that every time we do that we need to send a header to the browser with the type of file that we are sending.
+Then we just send the template.
+if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end(tempOverview);
+
+Now we already have our styled page with the placeholder, which we need to replace with the data from dataObj(json).
+We now need to loop over this dataObj (which is an array) and replace the placeholders in the template.
+
+We are looping with map because we want to return something that we store in the cardsHtml variable.
+
+In the callback, we are not going to write the code directly to replace the placeholders at each iteration, instead we create a function.
+
+This function will take the template card and the current element (product). In there, we are going to replace all of the placeholders. 
+
+We do this in a "chain" by setting the output to a let variable, so first we can replace the product name, than mutate the variable for the rest. Also, this is how we avoid the bad practice of directly manipulate the arguments that we pass into a function. 
+First we create a new variable and from there on we manipulate it, not the argument.
+Notice that, to replace, it is better to use a regex with the global flag instead of the template literal to assure that all instances are replaced.
+
+We also deal with the organic and not organic here. If the product is not organic, so false (remember that the value is a boolean), we want to replace the placeholder with the class of not-organic (sets the display to none). Remember that we are still in the temp overview.
+
+WHAT IS DONE SO FAR
+We loop over the dataObj that contains the array of object that came from the json file. At each iteration we replace the placeholders in the template card with the current product (remember that these cards are to be in the overview, but we created another template so we just inject the ones that are in the api).
+After the replaceTemplate function is called by the map method (called as many times as there are curr el), the loop will return with the five final html's. 
+The return is in the same format as the input, so an array with the five elements (strings). We don't want an array, so we just need to chain the join method after the map.
+
+Now we just need to replace the placeholder in the overview with this new string and respond with the output. 
+
+
+HOW TO PARSE VARIABLES FROM URL'S
+
+This is where we are going to use the url module that we've imported at the beginning.
+We start by logging two things to the console, the req.url and the result of using the module (url) calling parse on the url. console.log(url.parse(req.url, true)); We need to pass true in the parse method so that we can parse the query into an object.
+The query is the string ?id=0 and that is what we are parsing, that part from the url. 
+
+The url from the request is everything after the port (/product?id=0), and it changes when we click on the links, but we don't have any route like that (with the id basically).
+
+The result of parse is an object that contains information, among which is the query object that tells us the id. Remember that there is a more recent way to do this.
+
+Now we use destructuring so that we can extract the query and the pathname.
+
+With this is now time to build the product page using the template, this time we do not need to loop because there is only one product, all we need to do is to replace the data using the function that we've created with base on the product id.
+
+// Which product we want to display:
+    const product = dataObj[query.id];
+
+    // The output based on the previous data coming from product
+
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+
 */
+// Top level code, we could not do this inside of the callback that creates the server.
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
 );
+
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   "utf-8"
@@ -327,23 +455,55 @@ const tempCard = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
+// Slugify all the productName
+const slugs = dataObj.map((el) => {
+  return slugify(el.productName, { lower: true });
+}); // Now we could add each slug to the json and replace the id for the slug in the code.
+
 // Creating the server
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  // console.log(req.url);
+  const { query, pathname } = url.parse(req.url, true);
+  // const pathName = req.url;
 
   // OVERVIEW
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the overview");
+  if (pathname === "/" || pathname === "/overview") {
+    // to say the type of file we are sending
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    // Looping to replace the data
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    // Replacing
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+    // sending the file
+    res.end(output);
 
     // PRODUCT PAGE
-  } else if (pathName === "/product") {
-    res.end("This is the product");
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    // Which product we want to display:
+    const product = dataObj[query.id];
+
+    // The output based on the previous data coming from product
+
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     // API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
     });
+
     res.end(data); // Now we just send it.
 
     // NOT FOUND
